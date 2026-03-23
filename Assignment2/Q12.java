9911e2ebc34cb84e517e4c997e833bbe1187f7d5
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2026-03-23 07:17:02 +0530

    Assignment 2: 12th question

---
+++ Assignment2/Q12.java
@@ -0,0 +1,31 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Employee extends Person {
+    int eid;
+    double salary;
+
+    Employee(String name, int age, int eid, double salary) {
+        super(name, age); // Calls Person constructor
+        this.eid = eid;
+        this.salary = salary;
+    }
+
+    void empDisplay() {
+        System.out.println("Name: " + name + ", Age: " + age + ", EID: " + eid + ", Salary: " + salary);
+    }
+}
+
+public class Q12 {
+    public static void main(String[] args) {
+        Employee emp = new Employee("John Doe", 30, 101, 50000.0);
+        emp.empDisplay();
+    }
+}
