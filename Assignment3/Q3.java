cf709cff2cb8e40d07dd811470ffb8ac3a4c9611
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2026-04-01 19:50:32 +0530

    Assignment 3: 3rd question

---
+++ Assignment3/Q3.java
@@ -0,0 +1,35 @@
+class MarksOutOfBoundException extends Exception{
+    MarksOutOfBoundException(String s){
+        super(s);
+    }
+
+}
+
+
+class Student{
+    int m;
+    String n;
+
+    public Student(int m, String n) {
+        this.m = m;
+        this.n = n;
+    }
+
+    void disp(){
+        System.out.println("Name is "+n+" mark is "+m);
+    }
+}
+class Q3{
+    public static void main(String[] args) {
+        Student s1=new Student(99,"Ram");
+        try {
+            if(s1.m>100){
+                throw new MarksOutOfBoundException("More than 100");
+            }else{
+                s1.disp();
+            }
+        } catch (MarksOutOfBoundException e) {
+            System.out.println("Greater than 100"+e);
+        }
+    }
+}
