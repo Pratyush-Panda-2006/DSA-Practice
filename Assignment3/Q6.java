4c262d7dd1a8c512eacab40e1a9289c7dc1fd0a8
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2026-04-01 20:03:43 +0530

    Assignment 3: 6th  question

---
+++ Assignment3/Q6.java
@@ -0,0 +1,19 @@
+public class Q6 {
+    public static <T> int count(T[] array, T item) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null && array[i].equals(item)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Double[] grades = {9.5, 8.0, 7.5, 9.5, 10.0};
+        Double target = 9.5;
+
+        int occurrences = count(grades, target);
+        System.out.println("The value " + target + " appears " + occurrences + " times.");
+    }
+}
