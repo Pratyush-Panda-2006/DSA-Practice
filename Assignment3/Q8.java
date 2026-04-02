f385bf55a8204767ebbfe922901c16574c2faea1
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2026-04-02 22:25:53 +0530

    Assignment 3: 8th  question

---
+++ Assignment3/Q8.java
@@ -0,0 +1,11 @@
+public class Q8 {
+    public static double power(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * power(x, n - 1);
+    }
+    public static void main(String[] args) {
+        power(5.0,5);
+    }
+}
